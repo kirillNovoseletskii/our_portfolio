4fb861501be294551001f84fa8d717d5de14275c
Hell0W0rldD <49838571+Hell0W0rldD@users.noreply.github.com>
2019-04-23 20:50:59 +0300

    Языки, серв сторона
    Настройка языков на сайте. БЕТА

--- html/mainen.php
+++ html/mainen.php
@@ -1,50 +1,20 @@
 <!DOCTYPE html>
-
-<!---НЕ ИСПОЛЬЗУЕТСЯ!!! MAIN.PHP & INDEX.PHP & MAINEN.PHP -->
-
-
-<html lang="en">
 <head>
-    <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
-  <meta charset="UTF-8">
-  <link rel="stylesheet" href="../styles/first.css">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+<?php
+  $title = "Document";
+  require_once "../blocks/head.php";
+?>
 </head>
-<body>
-  <div class="mob-menu close_menu">
-    <div class="close">
-      <span class="close_line"></span>
-      <span class="close_line"></span>
-    </div>
-    <div class="links">
-        <ul class="nav-menu-mob">
-            <li><a href="#" class = 'mob_link'>MAIN</a></li>
-            <li><a href="#" class = 'mob_link'>PROJECTS</a></li>
-            <li><a href="#" class = 'mob_link'>FEEDBACK</a></li>
-            <li><a href="#" class = 'mob_link'>CONTACTS</a></li>
-          </ul>
-    </div>
 
-  </div>
-  <header class="header">
-    <h1 class="mob-title">PORTFOLIO</h1>
-    <div class="nav-mobile">
-      <span class="line"></span>
-      <span class="line"></span>
-      <span class="line"></span>
-    </div>
-    <ul class="nav">
-      <li><a href="#" class = 'header_link'>MAIN</a></li>
-      <li><a href="#" class = 'header_link'>PROJECTS</a></li>
-      <li><a href="#" class = 'header_link'>FEEDBACK</a></li>
-      <li><a href="#" class = 'header_link'>CONTACTS</a></li>
-    </ul>
-  </header>
+<body>
+
+  <?php
+    require_once '../blocks/header.php';
+  ?>
+
   <main class="main">
     <div class="about_us">
-      <h1 class="students">We are students, but can much...</h1>
+      <h1 class="students">We are students, but can do much...</h1>
       <p class="text students">Lorem ipsum dolor sit amet consectetur adipisicing elit. A, nihil ipsam. Praesentium necessitatibus iste aliquid pariatur voluptates cupiditate ipsa quae, cum delectus maiores laborum dicta molestias, aut officia libero dolores consectetur nam neque illo dolore voluptatum eveniet repellat! Iusto, ducimus minus, nam officia, qui consectetur nesciunt maiores veritatis autem amet dolor blanditiis sit obcaecati? Hic ullam ab, magni quae odit illo doloribus perferendis nihil, cumque fugit ut molestiae. Ullam amet praesentium, sit eveniet minima suscipit tenetur! Atque quae architecto repudiandae aliquam aliquid, provident quas commodi sequi accusantium ipsam, omnis tempore.</p>
     <h1 class = 'from'>Where are we from?</h1>
     <p class="text from">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum accusantium doloribus alias sit. Soluta numquam aliquam sint ex laborum quasi error labore perferendis harum, id, doloremque deserunt iure blanditiis laudantium alias. Sit quos, corporis libero voluptate magni ducimus. Eaque, voluptatibus non eos doloremque ad perspiciatis consequuntur nihil quo corrupti a aperiam, placeat quod unde veritatis sint, suscipit architecto ex! Velit quis fugit quia placeat modi eligendi vero dolores architecto. At modi debitis aperiam numquam doloremque quia obcaecati! Quasi veniam exercitationem odio fuga nostrum voluptas possimus, tempora fugiat asperiores delectus laborum saepe eum in esse ut pariatur distinctio a? Blanditiis, voluptatem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo quas necessitatibus sit accusamus aliquam, repellat non aut eveniet modi harum quod nemo ad! At quos earum culpa fugit fuga voluptatibus distinctio repudiandae eveniet nostrum pariatur numquam ad magni eos ducimus recusandae cum, magnam saepe ipsam esse nisi eum? Facere, nesciunt enim! Facere veniam voluptatum minima, velit deserunt temporibus tenetur nulla enim a officiis. Asperiores doloribus, suscipit velit maxime, laboriosam corrupti porro obcaecati, voluptates accusantium aspernatur perspiciatis. Dicta soluta commodi vel nostrum, quam expedita dignissimos ipsam incidunt reiciendis quibusdam hic impedit modi quos, blanditiis assumenda officia possimus veniam sunt, neque aperiam.</p>
@@ -63,17 +33,14 @@
       <h1 class = 'sk-title PHP_t'>PHP</h1></div>
     </div>
   </main>
-  <footer class="footer">
 
-      <ul class="soc">
-        <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-        <li><a href="#"><i class="fab fa-facebook-square"></i></a></li>
-        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-        <li><a href="#"><i class="fab fa-vk"></i></a></li>
-      </ul>
+  <?php
+    require_once '../blocks/footer.php';
+  ?>
 
-  </footer>
-  <script src="http://code.jquery.com/jquery-1.8.3.js"></script>
+  <!-- SCRIPT && STYLE -->
+
+<script src="http://code.jquery.com/jquery-1.8.3.js"></script>
 <script src="../JS/first.js"></script>
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 </body>
